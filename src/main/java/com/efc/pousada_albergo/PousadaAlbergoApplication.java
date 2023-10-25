7e6b3e440280419d9c7154ed32ed0431ac502f8c
eduardocaas <eduardofc.dev@gmail.com>
2023-10-25 13:33:59 -0300

    Initial commit

---
+++ src/main/java/com/efc/pousada_albergo/PousadaAlbergoApplication.java
@@ -0,0 +1,13 @@
+package com.efc.pousada_albergo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PousadaAlbergoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PousadaAlbergoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/efc/pousada_albergo/PousadaAlbergoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.efc.pousada_albergo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PousadaAlbergoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
