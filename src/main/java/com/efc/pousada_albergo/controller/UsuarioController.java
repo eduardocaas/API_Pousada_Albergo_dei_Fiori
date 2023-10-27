3d83bcb34884d6c8e960b95c1018bb98a911dd30
eduardocaas <eduardofc.dev@gmail.com>
2023-10-26 23:13:21 -0300

    Método salvar implementado

--- src/main/java/com/efc/pousada_albergo/controller/UsuarioController.java
+++ src/main/java/com/efc/pousada_albergo/controller/UsuarioController.java
@@ -1,16 +1,50 @@
 package com.efc.pousada_albergo.controller;
 
+import com.efc.pousada_albergo.domain.Usuario;
+import com.efc.pousada_albergo.service.UsuarioService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/usuario")
 public class UsuarioController {
 
-    @GetMapping
-    public ResponseEntity<String> test() {
-        return ResponseEntity.ok().body("1.. 2.. 3.. Testando... ");
+    private final UsuarioService service;
+
+    public UsuarioController(@Autowired UsuarioService service) {
+        this.service = service;
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<String> buscaPorId(@PathVariable Long id)
+    {
+
+    }
+
+    @PostMapping
+    public ResponseEntity<String> salvar(@Valid @RequestBody Usuario usuario)
+    {
+        try
+        {
+            Usuario obj = service.salvar(usuario);
+            URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+            return ResponseEntity.created(uri).build();
+        }
+        catch (HttpClientErrorException clientException)
+        {
+            return new ResponseEntity<>(clientException.getMessage(), clientException.getStatusCode());
+        }
+        catch (Exception exception)
+        {
+            return new ResponseEntity<>(exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
     }
 }
