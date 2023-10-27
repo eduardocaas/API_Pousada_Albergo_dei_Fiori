a6a29a7027b5aaed1eba1751e626613f016f6d0c
eduardocaas <eduardofc.dev@gmail.com>
2023-10-26 23:03:55 -0300

    Adicionado anotações de validações e métodos acessores

--- src/main/java/com/efc/pousada_albergo/domain/Usuario.java
+++ src/main/java/com/efc/pousada_albergo/domain/Usuario.java
@@ -2,6 +2,8 @@ package com.efc.pousada_albergo.domain;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import org.hibernate.validator.constraints.br.CPF;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
@@ -14,6 +16,7 @@ public class Usuario {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Getter
     @NotBlank(message = "Nome é obrigatório")
     @Column(nullable = false, length = 100)
     private String nome;
@@ -22,14 +25,18 @@ public class Usuario {
     @Column(nullable = false, length = 80)
     private String email;
 
+    @Getter
     @NotBlank(message = "Telefone é obrigatório")
     @Column(nullable = false, length = 50)
     private String telefone;
 
+    @CPF
+    @Getter
     @NotBlank(message = "CPF é obrigatório")
-    @Column(nullable = false, length = 11)
+    @Column(nullable = false, length = 11, unique = true)
     private String cpf;
 
+    @Getter
     @NotBlank(message = "Senha é obrigatória")
     @Column(nullable = false, length = 200)
     private String senha;
