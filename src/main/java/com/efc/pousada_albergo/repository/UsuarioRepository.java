14f12991b31d3cd13823882fb0ba685166817f59
eduardocaas <eduardofc.dev@gmail.com>
2023-10-26 23:04:21 -0300

    Criado Jpa repository para Usuário e implementado método de busca por CPF

---
+++ src/main/java/com/efc/pousada_albergo/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package com.efc.pousada_albergo.repository;
+
+import com.efc.pousada_albergo.domain.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Usuario findByCpf(String cpf);
+
+}
