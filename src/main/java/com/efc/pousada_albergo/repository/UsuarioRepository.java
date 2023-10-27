e1776937d668c071347643ab66aac95d0ce1c88e
eduardocaas <eduardofc.dev@gmail.com>
2023-10-26 23:08:06 -0300

    Refatorado método

--- src/main/java/com/efc/pousada_albergo/repository/UsuarioRepository.java
+++ src/main/java/com/efc/pousada_albergo/repository/UsuarioRepository.java
@@ -4,9 +4,11 @@ import com.efc.pousada_albergo.domain.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
-    Usuario findByCpf(String cpf);
+    Optional<Usuario> findByCpf(String cpf);
 
 }
