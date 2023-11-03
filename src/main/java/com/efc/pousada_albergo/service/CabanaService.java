5b6db5bcc39b76480b4585cc517bb96dc612a1e0
eduardocaas <eduardofc.dev@gmail.com>
2023-11-03 00:07:52 -0300

    Serviço para atualizar dominio cabana no banco de dados

---
+++ src/main/java/com/efc/pousada_albergo/service/CabanaService.java
@@ -0,0 +1,34 @@
+package com.efc.pousada_albergo.service;
+
+import com.efc.pousada_albergo.http.HttpConnector;
+import net.fortuna.ical4j.model.Calendar;
+import net.fortuna.ical4j.model.ComponentList;
+import net.fortuna.ical4j.model.Date;
+import net.fortuna.ical4j.model.component.VEvent;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CabanaService {
+
+    HttpConnector connector;
+
+    private void atualizaCabana1()
+    {
+        String icsUrl = "https://ical.booking.com/v1/export?t=004ab151-4832-475c-bb13-f9c73f5bb251";
+        Calendar calendar = connector.getCalendar(icsUrl);
+
+        ComponentList components = calendar.getComponents();
+
+        for (Object obj : components) {
+            if (obj instanceof VEvent) {
+                VEvent event = (VEvent) obj;
+                String summary = event.getSummary().getValue();
+                Date startDate = event.getStartDate().getDate();
+                Date endDate = event.getEndDate().getDate();
+                System.out.println(startDate);
+                System.out.println(endDate);
+            }
+        }
+    }
+
+}
