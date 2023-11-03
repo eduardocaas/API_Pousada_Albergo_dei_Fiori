914c55502b35812fd20b73bb3d1686865ea66f22
eduardocaas <eduardofc.dev@gmail.com>
2023-11-03 00:28:50 -0300

    Component Spring e scheduled task

--- src/main/java/com/efc/pousada_albergo/service/CabanaService.java
+++ src/main/java/com/efc/pousada_albergo/service/CabanaService.java
@@ -5,13 +5,16 @@ import net.fortuna.ical4j.model.Calendar;
 import net.fortuna.ical4j.model.ComponentList;
 import net.fortuna.ical4j.model.Date;
 import net.fortuna.ical4j.model.component.VEvent;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-@Service
+@Component
 public class CabanaService {
 
-    HttpConnector connector;
+    HttpConnector connector = new HttpConnector();
 
+    @Scheduled(cron = "0 * * * * *")
     private void atualizaCabana1()
     {
         String icsUrl = "https://ical.booking.com/v1/export?t=004ab151-4832-475c-bb13-f9c73f5bb251";
