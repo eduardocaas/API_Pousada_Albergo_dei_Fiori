62f374c535ef761fd483ebf4ee876a47f03818f8
eduardocaas <eduardofc.dev@gmail.com>
2023-11-05 13:06:16 -0300

    Endpoint para busca de datas nas tabelas cabana

--- src/main/java/com/efc/pousada_albergo/controller/CabanaController.java
+++ src/main/java/com/efc/pousada_albergo/controller/CabanaController.java
@@ -1,9 +1,19 @@
 package com.efc.pousada_albergo.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/cabana")
 public class CabanaController {
+
+    @GetMapping(value = "/{cabana}/{modeloData}")
+    public ResponseEntity<String> buscaDatasCabana(@PathVariable("cabana") Integer cabana,
+                                                   @PathVariable("modeloData") Integer modData)
+    {
+        
+    }
 }
