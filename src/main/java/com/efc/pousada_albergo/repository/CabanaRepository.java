dff1aba2dcce9cc4cac7a67f635ca95929d17443
eduardocaas <eduardofc.dev@gmail.com>
2023-11-05 00:02:33 -0300

    Efetuado ajuste no retorno do método da procedure

--- src/main/java/com/efc/pousada_albergo/repository/CabanaRepository.java
+++ src/main/java/com/efc/pousada_albergo/repository/CabanaRepository.java
@@ -1,14 +1,17 @@
 package com.efc.pousada_albergo.repository;
 
 
+import com.efc.pousada_albergo.domain.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.sql.Date;
 
-public interface CabanaRepository
+@Repository
+public interface CabanaRepository extends JpaRepository<Usuario, Integer>
 {
-
     @Query(value = "CALL sp_atualizaData(:cab, :di, :df)", nativeQuery = true)
-    void atualizaDataCabana(@Param("cab") Integer cabana_num, @Param("di") Date dataInicio, @Param("df") Date dataFim);
+    int atualizaDataCabana(@Param("cab") Integer cabana_num, @Param("di") Date dataInicio, @Param("df") Date dataFim);
 }
