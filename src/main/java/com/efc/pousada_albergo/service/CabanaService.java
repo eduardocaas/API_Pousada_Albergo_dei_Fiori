1ce436c46f07dc66e88dbc3f2b510dd53f5e9d5b
eduardocaas <eduardofc.dev@gmail.com>
2023-11-05 13:05:52 -0300

    Métodos para atualização de DB das tabelas cabana, refatorados

--- src/main/java/com/efc/pousada_albergo/service/CabanaService.java
+++ src/main/java/com/efc/pousada_albergo/service/CabanaService.java
@@ -12,6 +12,8 @@ import org.springframework.stereotype.Component;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class CabanaService {
@@ -30,9 +32,7 @@ public class CabanaService {
 
     SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
 
-    @Scheduled(cron = "0 * * * * *")
-    private void atualizaCabana1() throws ParseException {
-        String icsUrl = "https://ical.booking.com/v1/export?t=004ab151-4832-475c-bb13-f9c73f5bb251";
+    private void atualizaCabanaDB(Integer cabana, String icsUrl) throws ParseException {
         Calendar calendar = connector.getCalendar(icsUrl);
 
         ComponentList components = calendar.getComponents();
@@ -42,70 +42,32 @@ public class CabanaService {
                 VEvent event = (VEvent) obj;
                 String summary = event.getSummary().getValue();
                 String startDate = event.getStartDate().getDate().toString();
+                String endDate = event.getEndDate().getDate().toString();
 
                 java.util.Date parsedStartDate = dateFormat.parse(startDate);
                 java.sql.Date startDateSQL = new java.sql.Date(parsedStartDate.getTime());
-                String endDate = event.getEndDate().getDate().toString();
+
                 java.util.Date parsedEndDate = dateFormat.parse(endDate);
                 java.sql.Date endDateSQL = new java.sql.Date(parsedEndDate.getTime());
 
-                cabanaRepository.atualizaDataCabana(1, startDateSQL, endDateSQL);
+                cabanaRepository.atualizaDataCabana(cabana, startDateSQL, endDateSQL);
+
             }
         }
     }
 
     @Scheduled(cron = "0 * * * * *")
-    private void atualizaCabana2()
-    {
-        String icsUrl = "https://ical.booking.com/v1/export?t=642c91f5-2659-4965-a8ac-0990d98baa47";
-        Calendar calendar = connector.getCalendar(icsUrl);
-
-        ComponentList components = calendar.getComponents();
-
-        for (Object obj : components) {
-            if (obj instanceof VEvent) {
-                VEvent event = (VEvent) obj;
-                String summary = event.getSummary().getValue();
-                Date startDate = event.getStartDate().getDate();
-                Date endDate = event.getEndDate().getDate();
-            }
-        }
-    }
-
-    @Scheduled(cron = "0 * * * * *")
-    private void atualizaCabana3()
-    {
-        String icsUrl = "https://ical.booking.com/v1/export?t=df888a62-5a48-4be8-8330-9d2ba3accf9c";
-        Calendar calendar = connector.getCalendar(icsUrl);
-
-        ComponentList components = calendar.getComponents();
-
-        for (Object obj : components) {
-            if (obj instanceof VEvent) {
-                VEvent event = (VEvent) obj;
-                String summary = event.getSummary().getValue();
-                Date startDate = event.getStartDate().getDate();
-                Date endDate = event.getEndDate().getDate();
-            }
-        }
-    }
-
-    @Scheduled(cron = "0 * * * * *")
-    private void atualizaCabana4()
-    {
-        String icsUrl = "https://ical.booking.com/v1/export?t=4effcd71-229d-4097-8755-e28538c834b9";
-        Calendar calendar = connector.getCalendar(icsUrl);
-
-        ComponentList components = calendar.getComponents();
-
-        for (Object obj : components) {
-            if (obj instanceof VEvent) {
-                VEvent event = (VEvent) obj;
-                String summary = event.getSummary().getValue();
-                Date startDate = event.getStartDate().getDate();
-                Date endDate = event.getEndDate().getDate();
-            }
-        }
+    private void atualizaCabana1() throws ParseException {
+        String arrIcsUrl[];
+        arrIcsUrl = new String[4];
+        arrIcsUrl[0] = "https://ical.booking.com/v1/export?t=004ab151-4832-475c-bb13-f9c73f5bb251";
+        arrIcsUrl[1] = "https://ical.booking.com/v1/export?t=642c91f5-2659-4965-a8ac-0990d98baa47";
+        arrIcsUrl[2] = "https://ical.booking.com/v1/export?t=df888a62-5a48-4be8-8330-9d2ba3accf9c";
+        arrIcsUrl[3] = "https://ical.booking.com/v1/export?t=4effcd71-229d-4097-8755-e28538c834b9";
+        atualizaCabanaDB(1, arrIcsUrl[0]);
+        atualizaCabanaDB(2, arrIcsUrl[1]);
+        atualizaCabanaDB(3, arrIcsUrl[2]);
+        atualizaCabanaDB(4, arrIcsUrl[3]);
     }
 
 }
