745fae6ef9578cf0178de6aeac5f1ef559f2dc47
eduardocaas <eduardofc.dev@gmail.com>
2023-11-05 11:59:02 -0300

    RestController para cabana

---
+++ src/main/java/com/efc/pousada_albergo/controller/CabanaController.java
@@ -0,0 +1,9 @@
+package com.efc.pousada_albergo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cabana")
+public class CabanaController {
+}
