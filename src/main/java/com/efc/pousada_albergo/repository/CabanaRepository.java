57fcfac380444071725e2ede2c61c6c900c7970a
eduardocaas <eduardofc.dev@gmail.com>
2023-11-05 22:50:36 -0300

    Métodos provisórios para busca de datas por cabana

--- src/main/java/com/efc/pousada_albergo/repository/CabanaRepository.java
+++ src/main/java/com/efc/pousada_albergo/repository/CabanaRepository.java
@@ -8,10 +8,23 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.sql.Date;
+import java.util.List;
 
 @Repository
-public interface CabanaRepository extends JpaRepository<Usuario, Integer>
-{
+public interface CabanaRepository extends JpaRepository<Usuario, Integer> {
     @Query(value = "CALL sp_atualizaData(:cab, :di, :df)", nativeQuery = true)
     int atualizaDataCabana(@Param("cab") Integer cabana_num, @Param("di") Date dataInicio, @Param("df") Date dataFim);
+
+    @Query(value = "SELECT data FROM cabana_1", nativeQuery = true)
+    List<Date> buscaDataCabana1();
+
+    @Query(value = "SELECT data FROM cabana_2", nativeQuery = true)
+    List<Date> buscaDataCabana2();
+
+    @Query(value = "SELECT data FROM cabana_3", nativeQuery = true)
+    List<Date> buscaDataCabana3();
+
+    @Query(value = "SELECT data FROM cabana_4", nativeQuery = true)
+    List<Date> buscaDataCabana4();
+
 }
