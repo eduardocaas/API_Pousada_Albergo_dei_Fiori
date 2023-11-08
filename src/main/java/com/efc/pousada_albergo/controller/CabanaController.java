456b35e007afc662e5d76e1181a60173c35a4adf
eduardocaas <eduardofc.dev@gmail.com>
2023-11-08 14:34:17 -0300

    Envio de objecto JSON no corpo da resposta

--- src/main/java/com/efc/pousada_albergo/controller/CabanaController.java
+++ src/main/java/com/efc/pousada_albergo/controller/CabanaController.java
@@ -3,13 +3,11 @@ package com.efc.pousada_albergo.controller;
 import com.efc.pousada_albergo.service.CabanaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpClientErrorException;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/cabana")
@@ -22,14 +20,17 @@ public class CabanaController {
         this.service = service;
     }
 
+    @CrossOrigin(origins = "*")
     @GetMapping(value = "/{cabana}/{modeloData}")
-    public ResponseEntity<String> buscaDatasCabana(@PathVariable("cabana") Integer cabana,
+    public ResponseEntity buscaDatasCabana(@PathVariable("cabana") Integer cabana,
                                                    @PathVariable("modeloData") Integer modData)
     {
         try
         {
             String datas = service.buscaDataCabana(cabana, modData);
-            return ResponseEntity.ok().body(datas);
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("dates", datas);
+            return ResponseEntity.ok().body(map);
         }
         catch (HttpClientErrorException clientException)
         {
